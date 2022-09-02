ed7e0a581a1ea32511ae8a9e58cc50f230524173
Matilda Mared <matildamared@live.se>
2022-09-02 20:53:17 +0200

    add characters to use when generating password

--- src/logic/PasswordGenerator/PasswordGenerator.test.ts
+++ src/logic/PasswordGenerator/PasswordGenerator.test.ts
@@ -3,15 +3,12 @@ import {PasswordGenerator} from "./PasswordGenerator";
 
 describe("Password Generator", () => {
 	it("should return a password with the correct amount of characters", () => {
-		//arrange
 		const passwordGenerator = new PasswordGenerator();
 		const passwordLength = 4;
 
-		//act
 		const expected = 4;
 		const actual = passwordGenerator.generate();
 
-		//assert
 		expect(actual.length).toBe(expected);
 	});
 });
---
+++ src/logic/characters/characters.ts
@@ -0,0 +1,6 @@
+export const characters = {
+	lowercase: "abcdefghijklmnopqrstuvwxyz",
+	uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+	numbers: "0123456789",
+	symbols: "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~",
+};

1a0da85298b6265af6555e05964996a5c7587214
Matilda Mared <matildamared@live.se>
2022-09-08 19:00:53 +0200

    add css reset

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import GlobalStyles from "./styles/globalStyles";
 import Variables from "./styles/variables";
+import Reset from "./styles/reset";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
 		<GlobalStyles/>
-		<Variables />
+		<Variables/>
+		<Reset/>
 		<App/>
 	</React.StrictMode>
 );

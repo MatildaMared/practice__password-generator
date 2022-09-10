faad845560504fe3040f735f1a7bdc3bdd565659
Matilda Mared <matildamared@live.se>
2022-09-10 16:42:22 +0200

    add formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import {useState} from "react";
+import { useState } from "react";
 import styled from "styled-components";
 import PasswordContainer from "./components/PasswordContainer";
-import {Password} from "./interfaces/Password";
+import { Password } from "./interfaces/Password";
 
 function App() {
 	const [password, setPassword] = useState(Password.Initial);
@@ -10,24 +10,24 @@ function App() {
 		<Container>
 			<h1 className="heading-medium">Password Generator</h1>
 			<Content>
-				<PasswordContainer password={password}/>
+				<PasswordContainer password={password} />
 			</Content>
 		</Container>
 	);
 }
 
 const Container = styled.div`
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  padding: 16px;
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	padding: 16px;
 `;
 
 const Content = styled.div`
-  width: 100%;
-  max-width: 540px;
+	width: 100%;
+	max-width: 540px;
 `;
 
 export default App;
